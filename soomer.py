e14d209325e97c40c2f8530cfb504d0bf14cc27b
Gopal Rozendal <gprozendal@gmail.com>
2020-11-12 19:56:13 -0500

    Cleanup old code

--- soomer.py
+++ soomer.py
@@ -19,13 +19,4 @@ async def on_message(message):
         await message.channel.send('Yeet')
         print(message.guild)
 
-
-
-# class MyClient(discord.Client):
-#     async def on_ready(self):
-#         print('Logged on as {0}!'.format(self.user))
-
-#     async def on_message(self, message):
-#         print('Message from {0.author}: {0.content}'.format(message))
-
 client.run('$YOURTOKENHERE')
