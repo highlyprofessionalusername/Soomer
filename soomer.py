4c97c9dd851ad1a987a71a4bc5d22642067a7d4f
Gopal Rozendal <gprozendal@gmail.com>
2020-11-12 21:04:51 -0500

    sqlite db

--- soomer.py
+++ soomer.py
@@ -1,5 +1,13 @@
 import discord
 import logging
+import re
+import sqlite3
+
+conn = sqlite3.connect('emojilytics.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS emojidata (emoji text, message_used integer, reacc_used integer)''')
+conn.commit()
+
 # logging.basicConfig(Level=logging.WARNING)
 
 client = discord.Client()
@@ -8,15 +16,14 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     for i in range(len(client.guilds)):
-        print(client.guilds[i])
+        print("logged into server " + str(client.guilds[i]))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith('42069') and str(message.guild) == "Testing server for cool kids only":
-        await message.channel.send('Yeet')
+    if message.content.startswith('42069'):
         print("this message sent from "+ str(message.guild))
-        print(message)
+        print()
 
 client.run('$YOURTOKENHERE')
