d6e3aa0bc5b98c1d9da6453decfa41d72df3f14e
Gopal Rozendal <gprozendal@gmail.com>
2020-11-19 22:18:45 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='soomer',
+    version='0.1.0',
+    author='Thicculi',
+    author_email='gprozendal@gmail.com',
+    packages=['Soomer', 'towelstuff.test'],
+    description='65000 years of shitposting',
+    install_requires=[
+        "discord",
+    ],
+)
