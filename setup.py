5d3c9ad151f1c121990ee1d16744884d511521cd
Gopal Rozendal <gprozendal@gmail.com>
2020-11-19 22:19:51 -0500

    Cleanup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Thicculi',
     author_email='gprozendal@gmail.com',
-    packages=['Soomer', 'towelstuff.test'],
+    packages=['Soomer'],
     description='65000 years of shitposting',
     install_requires=[
         "discord",
